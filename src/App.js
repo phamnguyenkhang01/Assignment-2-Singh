afd9b294892a5b9b7792d8adaa5584f61b8f77f6
phamnguyenkhang01 <60402428+phamnguyenkhang01@users.noreply.github.com>
2022-02-13 03:09:08 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Login from "./Login";
+import Signup from "./Signup";
+import SignupDetail from "./SignupDetail";
+
+class App extends Component {
+  render() {
+    return <SignupDetail></SignupDetail>;
+  }
+}
+
+export default App;
